9d0c25a1dfcb13e5dd38068960ad85d0b3352fa1
tbvjqkdlcm1 <tbvjqkdlcm1@naver.com>
2024-12-14 07:20:39 +0900

    _11726_2n타일링2

--- algorithm/_11726_2n타일링2.java
+++ algorithm/_11726_2n타일링2.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class _11726_2n타일링 {
+public class _11726_2n타일링2 {
     private static int n;
     private static int[] arr;
 
@@ -14,10 +14,10 @@ public class _11726_2n타일링 {
 
         arr = new int[1001];
         arr[1] = 1;
-        arr[2] = 2;
+        arr[2] = 3;
 
         for (int i=3; i<=n; i++) {
-            arr[i] = (arr[i-1] + arr[i-2]) % 10007;
+            arr[i] = (arr[i-1] + arr[i-2]*2) % 10007;
         }
         System.out.println(arr[n]);
     }
